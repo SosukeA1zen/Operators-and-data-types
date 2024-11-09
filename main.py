a3208836cecc529092c3f2087fb58f5b9631f06f
SosukeA1zen <samkymanthi@gmail.com>
2024-11-09 20:44:59 +0300

    Add files via upload
    Operators and data types activity

---
+++ main.py
@@ -0,0 +1,26 @@
+# name="Samuel"
+# age=16
+# isStudent=True
+# marks=52.5
+
+# print("My name is ",name)
+# print("data type for name",type(name))
+# print("My age is",age)
+# print("data type for age",type(age))
+# print("data type for Student",type(isStudent))
+# print("data type for marks",type(marks))
+
+num1=10
+num2=20
+
+print("addition",num1+num2)
+print("subtraction",num2-num1)
+print("multiplication",num1*num2)
+print("division",num2/num1)
+print("remainder",num2%num1)
+print("power",num1**2)
+print("power",num2**2)
+print("equality",num1==num2)
+print("not equal to",num1!=num2)
+print("greater than",num2>=num1)
+print("less than",num1<=num2)
